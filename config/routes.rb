254956f030f74b881a6165096cfcca54586d13f0
sboursen <dev.sboursen@gmail.com>
2022-07-05 11:17:57 +0100

    Build the Greeting resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  scope '/api' do
+    resources :greetings, only: [:index]
+  end
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "greetings#index"
 end
